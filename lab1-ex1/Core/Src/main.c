44a222cb72676644902f348c401d84b28cbaeba4
AnhPLT3 <_Anh.pham.ext.AZ@sodexo.com>
2025-09-26 13:17:50 +0700

    finish ext1

--- lab1-ex1/Core/Src/main.c
+++ lab1-ex1/Core/Src/main.c
@@ -97,6 +97,15 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	HAL_GPIO_TogglePin ( GPIOA , GPIO_PIN_5 ) ;
+	if(HAL_GPIO_ReadPin(GPIOA , GPIO_PIN_5) == GPIO_PIN_SET){
+		HAL_GPIO_WritePin ( GPIOA , GPIO_PIN_6 , GPIO_PIN_RESET ) ;
+		HAL_Delay(1000);
+	}
+	else{
+		HAL_GPIO_WritePin ( GPIOA , GPIO_PIN_6 , GPIO_PIN_SET ) ;
+		HAL_Delay(1000);
+	}
   }
   /* USER CODE END 3 */
 }
